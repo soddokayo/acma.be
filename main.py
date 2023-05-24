ef8b4afd1b0b4f3ee8932f566e02c325030fb8a1
soddokayo <soddokayo@gmail.com>
2023-05-24 19:45:03 +0900

    fixed cors issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 import secrets, os, json
@@ -6,6 +7,16 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 load_dotenv(verbose=True)
 mongoURL = os.getenv('MONGODB_URL')
